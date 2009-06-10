fefa674fe8427013c81246348fa8e154470f81b0
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-06-10 14:52:07 +0900

    initial commit

---
+++ lib/Net/Drizzle.pm
@@ -0,0 +1,33 @@
+package Net::Drizzle;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Net::Drizzle -
+
+=head1 SYNOPSIS
+
+  use Net::Drizzle;
+
+=head1 DESCRIPTION
+
+Net::Drizzle is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom @*(#RJKLFHFSDLJF gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
