99f6f4443a3b9e53a3c9873667c6aa45de14808e
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-06-15 10:43:13 +0900

    use method chain

--- examples/simple.pl
+++ examples/simple.pl
@@ -1,10 +1,10 @@
 use strict;
 use warnings;
-use Net::Drizzle;
+use Net::Drizzle ':constants';
 
-my $con = Net::Drizzle::Connection->new;
-$con->add_options(Net::Drizzle::DRIZZLE_CON_MYSQL);
-$con->set_db("information_schema");
+my $con = Net::Drizzle::Connection->new
+                                  ->add_options(DRIZZLE_CON_MYSQL)
+                                  ->set_db("information_schema");
 my $s = $con->query_str("SELECT table_schema,table_name FROM tables");
 $s->buffer;
 
